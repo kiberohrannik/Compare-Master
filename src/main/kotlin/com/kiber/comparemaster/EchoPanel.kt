f37617aa71ebdb4dd1fdbcd38e41435984b65111
kiberohrannik <vikulik2409@gmail.com>
2024-01-29 23:43:30 +0100

    add simple json formatting ('to-pretty', 'to-raw' options)

--- src/main/kotlin/com/kiber/comparemaster/EchoPanel.kt
+++ src/main/kotlin/com/kiber/comparemaster/EchoPanel.kt
@@ -1,5 +1,6 @@
 package com.kiber.comparemaster
 
+import com.intellij.json.JsonFileType
 import com.intellij.openapi.application.runUndoTransparentWriteAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
@@ -15,27 +16,50 @@ class EchoPanel(project: Project) : JPanel(BorderLayout()) {
     private var echo = "ECHO\n"
 
     init {
-        val leftEditor = createEmptyEditor(project)
-        val rightEditor = createEmptyEditor(project)
+        val jsonFormatter = JsonFormatter()
 
-        val leftPanel = createEditorPanel(leftEditor)
-        val rightPanel = createEditorPanel(rightEditor)
+        val json = """{"menu":{"header":"SVG Viewer","items":[{"id":"Open"},{"id":"OpenNew","label":"Open New"}]}}"""
+
+        val leftEditor = EditorFactory.getInstance()
+            .createEditor(DocumentImpl(json), project, JsonFileType.INSTANCE, false)
+
+        val beautifyButton = JButton("Beautify")
+        beautifyButton.addActionListener {
+            runUndoTransparentWriteAction {
+                val prettyJson = jsonFormatter.toPrettyJson(leftEditor.document.text)
+                leftEditor.document.replaceString(0, leftEditor.document.text.length, prettyJson)
+            }
+        }
+
+        val uglifyButton = JButton("Uglify")
+        uglifyButton.addActionListener {
+            runUndoTransparentWriteAction {
+                val rawJson = jsonFormatter.toRawJson(leftEditor.document.text)
+                leftEditor.document.replaceString(0, leftEditor.document.text.length, rawJson)
+            }
+        }
+
+        val leftPanel = createEditorPanel(leftEditor, listOf(beautifyButton, uglifyButton))
+
+        val rightEditor = createEmptyEditor(project)
+        val echoButton = JButton("Echo!")
+        echoButton.addActionListener { showEcho(rightEditor) }
+
+        val rightPanel = createEditorPanel(rightEditor, listOf(echoButton))
 
         val splitPane = EchoSplitPaneFactory().createSplitPane(leftPanel, rightPanel)
 
         add(splitPane, BorderLayout.CENTER)
     }
 
-
-    private fun createEditorPanel(editor: Editor): JPanel {
-        val button = JButton("Echo!")
-        button.addActionListener { showEcho(editor) }
-
+    private fun createEditorPanel(editor: Editor, buttons: List<JButton>): JPanel {
         val panel = JPanel()
             .apply {
                 layout = BoxLayout(this, BoxLayout.PAGE_AXIS)
                 add(editor.component)
-                add(button)
+                for (button in buttons) {
+                    add(button)
+                }
             }
 
         return panel
---
+++ src/main/kotlin/com/kiber/comparemaster/JsonFormatter.kt
@@ -0,0 +1,16 @@
+package com.kiber.comparemaster
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jetbrains.rd.util.string.printToString
+
+class JsonFormatter {
+    private val objectMapper = ObjectMapper()
+
+    fun toPrettyJson(json: String): String {
+        return objectMapper.readTree(json).toPrettyString()
+    }
+
+    fun toRawJson(json: String): String {
+        return objectMapper.readTree(json).toString()
+    }
+}
