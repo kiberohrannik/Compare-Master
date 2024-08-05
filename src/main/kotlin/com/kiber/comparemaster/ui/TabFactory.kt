ba874f7a3fcd1f54de1f7de8252849a9a8bc904b
kiberohrannik <vikulik2409@gmail.com>
2024-08-05 19:27:36 +0200

    add removing of tabs

--- src/main/kotlin/com/kiber/comparemaster/ui/TabFactory.kt
+++ src/main/kotlin/com/kiber/comparemaster/ui/TabFactory.kt
@@ -4,21 +4,31 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.content.ContentFactory
 import com.kiber.comparemaster.ToolWindowPanel
+import org.jetbrains.kotlin.analysis.api.impl.barebone.annotations.NotThreadSafe
 
+@NotThreadSafe
 object TabFactory {
 
-    private val TABS = arrayOf("Tab-A", "Tab-B", "Tab-C", "Tab-D", "Tab-E", "Tab-F")
-    private val tabsIterator: Iterator<String> = TABS.iterator()
+    private const val MAX_TAB_NUMBER = 9
+    private var counter = 0
 
     fun createTab(project: Project, toolWindow: ToolWindow) {
 
-        if(tabsIterator.hasNext()) {
-            val tabName = tabsIterator.next()
+        if(canAddNewTab(toolWindow)) {
+            val tabName = "TAB-${++counter}"
             val panel = ToolWindowPanel(project)
             val content = ContentFactory.getInstance().createContent(panel, tabName, false)
+            content.isCloseable = true
 
             toolWindow.contentManager.addContent(content)
             toolWindow.contentManager.setSelectedContent(content)
         }
     }
+
+
+    private fun canAddNewTab(toolWindow: ToolWindow) =
+        toolWindow.contentManager.contents
+            .map { c -> c.component }
+            .filterIsInstance<ToolWindowPanel>()
+            .count() < MAX_TAB_NUMBER
 }
