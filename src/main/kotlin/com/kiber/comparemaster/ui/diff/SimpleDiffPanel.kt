ec5e2a1a0ef021b390aa8a9813f3e86704f26cf4
kiberohrannik <vikulik2409@gmail.com>
2025-02-10 18:10:40 +0100

    fix issue with intellij internal api usage

--- src/main/kotlin/com/kiber/comparemaster/ui/diff/SimpleDiffPanel.kt
+++ src/main/kotlin/com/kiber/comparemaster/ui/diff/SimpleDiffPanel.kt
@@ -2,23 +2,25 @@ package com.kiber.comparemaster.ui.diff
 
 import com.intellij.diff.DiffContentFactory
 import com.intellij.diff.DiffDialogHints
+import com.intellij.diff.DiffManager
 import com.intellij.diff.chains.SimpleDiffRequestChain
-import com.intellij.diff.impl.DiffWindow
 import com.intellij.diff.requests.SimpleDiffRequest
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
-object SimpleDiffPanel: DiffView {
+object SimpleDiffPanel : DiffView {
 
     private val diffContentFactory = lazy { DiffContentFactory.getInstance() }
 
 
     override fun showDiff(project: Project, left: VirtualFile, right: VirtualFile) {
 
-        val diffReq = SimpleDiffRequest(diffViewTitle,
+        val diffReq = SimpleDiffRequest(
+            diffViewTitle,
             diffContentFactory.value.create(project, left),
-            diffContentFactory.value.create(project, right), left.name, right.name)
+            diffContentFactory.value.create(project, right), left.name, right.name
+        )
 
-        DiffWindow(project, SimpleDiffRequestChain(diffReq), DiffDialogHints.NON_MODAL).show()
+        DiffManager.getInstance().showDiff(project, SimpleDiffRequestChain(diffReq), DiffDialogHints.NON_MODAL);
     }
 }
